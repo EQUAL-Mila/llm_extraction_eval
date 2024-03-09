16ed1a9ad5088d27524f8d59c7f1496785a99aa4
sert121 <yashrajmore29@gmail.com>
2024-03-09 18:15:48 -0500

    added vlm model support

--- pythia_utils.py
+++ pythia_utils.py
@@ -1,24 +1,40 @@
 import os
-from transformers import GPTNeoXForCausalLM, AutoTokenizer
+from transformers import AutoTokenizer, GPTNeoXForCausalLM
+from transformers import GPTNeoXConfig
+import vllm
+from dotenv import load_dotenv
+load_dotenv()
+
 path_to_scratch = os.environ.get("SCRATCH")
 
-def load_pythia_model(modelsize, modelstep, device='cpu', padding_side='right', truncation_side='right', model_max_length=2048):
-    model = GPTNeoXForCausalLM.from_pretrained(
-        "EleutherAI/%s" % modelsize,
-        revision=modelstep,
-        cache_dir=path_to_scratch + "/%s/%s/" % (modelsize, modelstep),
-    )
-    model = model.to(device)
 
-    tokenizer = AutoTokenizer.from_pretrained(
-        "EleutherAI/%s" % modelsize,
-        revision=modelstep,
-        cache_dir=path_to_scratch + "/%s/%s/" % (modelsize, modelstep),
-        padding_side=padding_side, truncation_side=truncation_side, model_max_length=model_max_length
-    )
+def cache_check_tokenizer(modelsize, modelstep,):
+    '''
+    Check if the tokenizer is already cached/saved
+    '''
+    if os.path.exists(path_to_scratch + "/%s/%s/" % (modelsize, modelstep) + "tokenizer_config.json"):
+        return True
+    
+def load_pythia_model(modelsize, modelstep, device='cuda', padding_side='right', truncation_side='right', model_max_length=2048):
+
+    # check if the tokenizer is already cached/saved
+    if not cache_check_tokenizer(modelsize, modelstep):
+        tokenizer = AutoTokenizer.from_pretrained(
+            "EleutherAI/%s" % modelsize,
+            revision=modelstep,
+            padding_side=padding_side, truncation_side=truncation_side, model_max_length=model_max_length
+        )        
+        tokenizer.save_pretrained(path_to_scratch + "/%s/%s/" % (modelsize, modelstep))
+    
+    model = vllm.LLM(model = f"EleutherAI/{modelsize}", revision = modelstep ,tokenizer= path_to_scratch + "/%s/%s/" % (modelsize, modelstep), trust_remote_code = True)
+
+    # testing model outputs
+    l = model.generate("Hello, eincorp!")
+    print(l)
 
     return model, tokenizer
 
+
 if __name__=="__main__":
     model, tokenizer = load_pythia_model('pythia-1.4b', 'step100000')
-    print(model)
+
