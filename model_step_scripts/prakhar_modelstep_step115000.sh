dba5e8e1caebb3ce56c023ab888ec3e8b45b7124
sert121 <yashrajmore29@gmail.com>
2024-05-03 15:09:20 -0400

    .

---
+++ model_step_scripts/prakhar_modelstep_step115000.sh
@@ -0,0 +1,15 @@
+
+#!/bin/bash
+
+#SBATCH --job-name=step115000_pythia_run
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.err
+#SBATCH --time=15:00:00
+#SBATCH --mem=48000M
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=prakhar.ganesh@mila.quebec
+
+source activate pythia
+python experiment.py --evalfile finalidx100000.csv --complen 500 --maxtokens 500 --modelstep step115000 
+echo "Done!"
