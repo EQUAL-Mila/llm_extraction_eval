4aec6f2cfdb307fae1ad368ab0b5489daf52bfba
sert121 <yashrajmore29@gmail.com>
2024-05-06 05:08:22 -0400

    add filtration for books outside p

--- bookcheck.py
+++ bookcheck.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from datasets import load_dataset
-
+from tqdm import tqdm
 
 def preprocess_books(dataset='books1'):
     '''
@@ -25,25 +25,82 @@ def preprocess_books(dataset='books1'):
         for item in cleaned_dataset:
             f.write("%s\n" % item)
 
+
 def load_pg_metadata():
-    data = pd.read_csv('pg_catalog.csv', header=None, names=['Title'])
+    # complete gutenberg catalog/metadata
+
+    data = pd.read_csv('pg_catalog.csv')
+    data = data['Title']
     data = data.tolist()
     data = [i.lower() for i in data]
     return data
 
+
 def load_pg19_metadata():
-    dataset = load_dataset("pg19")
-    # iterate through the huggingface dataset and extract only the first columns
-    metadata = []
-    for i in range(len(dataset['train'])):
-        l = dataset['train'][i]['short_book_title']
-        l = l.lower()
-        metadata.append(l)
+
+    data = pd.read_csv('./pg19_nineteen_metadata.csv', header=None)
+
+    print("Number of columns:", data.shape[1])
+
+    # Print the column headers
+    print("Column headers:", data.columns)
+
+    second_column = data.iloc[:,
+                              1]  # ':' means all rows, '1' is the index of the second column
+
+    data = second_column.tolist()
+    data = [i.lower() for i in data]
+
     # save the metadata to a text file
     with open('pile_metadata/pg19.txt', 'w') as f:
-        for item in metadata:
+        for item in data:
             f.write("%s\n" % item)
+
+
+def get_books_outside_pile():
+
+    # read all three texts files in the pile subfolder
+    with open('pile_metadata/books1_cleaned.txt', 'r') as f:
+        books1 = f.read().splitlines()
+    with open('pile_metadata/books3_cleaned.txt', 'r') as f:
+        books3 = f.read().splitlines()
+
+    with open('pile_metadata/pg19.txt', 'r') as f:
+        pg19 = f.read().splitlines()
+
+    big_pile = books1 + books3 + pg19
+    print("the length of the big pile is: ")
+    print(len(big_pile))
+
+    # read the pg_catalog.csv file
+    pg_catalog = load_pg_metadata()
+
+    #identify books that are not in the pile buit are in the pg_catalog
+    books_outside_pile = []
+    for book in tqdm(pg_catalog):
+        book_names = book.lower().split()
+        number_of_pieces = len(book_names)
+        detected_count = 0
+
+        for sentence in big_pile:
+            if book in sentence:
+                detected_count += 1
+                break
+            # if detected_count / number_of_pieces > 0.75:
+            #     continue
+
+        if detected_count == 0:
+            books_outside_pile.append(book)
+            # save the books outside the pile to a text file
+            with open('pile_metadata/books_outside_pile.txt', 'w') as f:
+                for item in books_outside_pile:
+                    f.write("%s\n" % item)
+
+    print("\n -- Some stats -- \n")
+    print("Books outside pile:", len(books_outside_pile))
+    print("Total books in pile:", len(big_pile))
 
 if __name__ == '__main__':
     # preprocess_books('books3')
-    load_pg19_metadata()
+    # load_pg19_metadata()
+    get_books_outside_pile()
