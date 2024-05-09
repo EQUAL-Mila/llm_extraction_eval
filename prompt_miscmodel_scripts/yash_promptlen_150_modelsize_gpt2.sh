00b9aab1494867acdd2b4f4755e3c5299940332f
sert121 <yashrajmore29@gmail.com>
2024-05-09 17:46:49 -0400

    prompt misc models vs len

---
+++ prompt_miscmodel_scripts/yash_promptlen_150_modelsize_gpt2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=150_miscmodel_run
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.err
+#SBATCH --time=15:00:00
+#SBATCH --mem=32000M
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=prakhar.ganesh@mila.quebec
+
+module load miniconda/3
+conda activate vllm
+python experiment.py --evalfile finalidx100000.csv --complen 500 --maxtokens 500 --promptlen 150 --modelsize gpt2
+echo "Done!"
