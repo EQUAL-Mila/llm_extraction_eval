ba5af088b74cda8291698989058a9fc6b0af6102
sert121 <yashrajmore29@gmail.com>
2024-05-13 20:42:38 -0400

    add deduped sh

---
+++ prompt_len_scripts_deduped/prakhar_promptlen_150.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=150_pythia_run
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.err
+#SBATCH --time=20:00:00
+#SBATCH --mem=32000M
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=prakhar.ganesh@mila.quebec
+
+source activate pythia
+python experiment.py --evalfile finalidx100000.csv --complen 500 --maxtokens 500 --promptlen 150 --modelsize pythia-6.9b-deduped
+echo "Done!"
