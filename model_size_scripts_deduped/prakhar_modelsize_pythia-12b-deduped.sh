d1e7a82fab5f1acf9c9b19bcadd7eef55f3636f3
sert121 <yashrajmore29@gmail.com>
2024-05-14 20:34:18 -0400

    addededupedstuff

---
+++ model_size_scripts_deduped/prakhar_modelsize_pythia-12b-deduped.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=pythia-12b-deduped_pythia_run
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.err
+#SBATCH --time=15:00:00
+#SBATCH --mem=32000M
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=prakhar.ganesh@mila.quebec
+
+source activate pythia
+python experiment.py --evalfile finalidx100000.csv --complen 500 --maxtokens 500 --modelsize pythia-12b-deduped 
+echo "Done!"
