ad738eb46c4c2e4156141f9dfe311c15d78ebf0f
sert121 <yashrajmore29@gmail.com>
2024-08-06 05:28:20 -0400

    changes to exp.py

--- experiment.py
+++ experiment.py
@@ -8,11 +8,20 @@ import wandb
 import random
 
 from model_utils import load_pythia_model, VLLMModelWrapper, load_llama_together, load_mpt_7b, load_phi, load_falcon, load_gpt2
-from model_utils import load_gemma_7, load_gemma_2, load_redpajama_base
+from model_utils import load_gemma_7, load_gemma_2, load_redpajama_base, load_olmo
+from model_utils import load_gpt_large, load_gpt_xl, load_opt_6b
 
 from prompt_loader import ExtractionPromptDataset
 from utils import setup_parser, get_filename, get_instruction_ids, get_mask_token_id
 
+import numpy as np
+from cached_path import cached_path
+
+from olmo.config import TrainConfig
+from olmo.data import build_memmap_dataset
+
+from tqdm import tqdm
+import time
 from transformers import logging
 from transformers import AutoTokenizer
 logging.set_verbosity_error()
@@ -20,8 +29,10 @@ path_to_scratch = "/network/scratch/p/prakhar.ganesh/"
 
 
 def single_eval_run(args):
-    device = "cuda" if torch.cuda.is_available() else "cpu"
 
+    start = time.time()
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print("...1. loading model")
     if 'pythia' in args.modelsize:
         model = load_pythia_model(args.modelsize,
                                   args.modelstep,
@@ -43,6 +54,14 @@ def single_eval_run(args):
         model = load_gemma_2()
     if 'gemma7' in args.modelsize:
         model = load_gemma_7()
+    if 'opt6b' in args.modelsize:
+        model = load_opt_6b()
+    if 'gptlarge' in args.modelsize:
+        model = load_gpt_large()
+    if 'gptxl' in args.modelsize:
+        model = load_gpt_xl()
+    if 'olmo' in args.modelsize:
+        model = load_olmo(modelstep=args.modelstep)
 
     model = VLLMModelWrapper(model=model,
                              temperature=args.temperature,
@@ -50,6 +69,11 @@ def single_eval_run(args):
                              use_beam_search=not args.sampling,
                              max_tokens=args.maxtokens)
 
+    end = time.time()
+    print(f"Time taken to load model: {end-start}")
+
+    print("\n...2. loading prompt dataset")
+    start = time.time()
     if 'deduped' in args.modelsize:
         pilepath = path_to_scratch + "/pile-pythia/pile-deduped/document"
     else:
@@ -57,36 +81,50 @@ def single_eval_run(args):
 
     tokenizer = AutoTokenizer.from_pretrained("EleutherAI/pythia-1.4b")
 
+    dataset_type = 'pythia'
+    dataset_type = 'olmo' if 'olmo' in args.modelsize else dataset_type
+    
     prompt_dataset = ExtractionPromptDataset(
         pilepath=pilepath,
         evalfile=args.evalfile,
         promptlen=args.promptlen,
         complen=args.complen,
         prompttype=args.prompttype,
-        instructions=get_instruction_ids(args.instructions),
+        instructions=get_instruction_ids(args.instructions,
+                                         dataset_type=dataset_type),
         window=args.window,
-        mask_token=get_mask_token_id(args.modelsize))
+        mask_token=get_mask_token_id(args.modelsize,
+                                     dataset_type=dataset_type),
+        dataset_type=dataset_type)
 
+    print(f"Length of prompt dataset: {len(prompt_dataset)}")
+    print("batchsize:", args.batchsize)
     prompt_loader = torch.utils.data.DataLoader(prompt_dataset,
                                                 batch_size=args.batchsize,
                                                 shuffle=False)
 
+    end = time.time()
+    print(f"Time taken to load prompt dataset: {end-start}")
+
     gen_arr = []
-                    
-                    
+
     counter = 0
+    print("\n...3. generating completions")
+    start = time.time()
+
     for sample in tqdm(prompt_loader):
         prompt_ids, completion_ids = sample['prompt'].detach().cpu().tolist(
         ), sample['completion'].detach().cpu().tolist()
 
-        if "pythia" in args.modelsize:
+        if "pythia" in args.modelsize or "olmo" in args.modelsize:
             outgen = model.generate_text(prompt_token_ids=prompt_ids)
             outgen_ids = [ele.outputs[0].token_ids for ele in outgen]
+
         else:
             prompts = tokenizer.batch_decode(np.array(prompt_ids))
             outgen = model.generate_text(prompts=prompts)
             outgen = [str(ele.outputs[0].text) for ele in outgen]
-            outgen_ids = tokenizer.encode(outgen, is_split_into_words=True)
+            outgen_ids = tokenizer(outgen)
             # for ele in outgen:
             #     print(ele)
             #     outgen_ids = tokenizer.encode(ele)
@@ -99,9 +137,12 @@ def single_eval_run(args):
         counter += 1
         wandb.log({'counter': counter})
 
-    with open(path_to_scratch + '/extraction_results/' + get_filename(
-             args, args_ignore=['scoring', 'batchsize', 'numgpus']),
-             "wb") as fp:
+    end = time.time()
+    print(f"Time taken to generate completions: {end-start}")
+    with open(
+            path_to_scratch + '/extraction_results/' + get_filename(
+                args, args_ignore=['scoring', 'batchsize', 'numgpus']),
+            "wb") as fp:
         pickle.dump(gen_arr, fp)
 
 
